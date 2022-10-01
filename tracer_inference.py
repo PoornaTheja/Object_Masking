fb0dddd753a36065ac21f195800e2d07f0e8b390
PoornaTheja <ramesh.poornatheja.cse20@itbhu.ac.in>
2022-10-01 19:10:10 +0530

    tracer inference update 1.1

--- tracer_inference.py
+++ tracer_inference.py
@@ -39,8 +39,8 @@ class Inference():
                                       batch_size=args.batch_size, shuffle=False,
                                       num_workers=args.num_workers, transform=self.test_transform)
 
-        # if args.save_map is not None:
-        #     os.makedirs(os.path.join('mask', self.args.dataset), exist_ok=True)
+        if args.save_map is not None:
+            os.makedirs(os.path.join('data', 'tracer_results'), exist_ok=True)
         #     os.makedirs(os.path.join('object', self.args.dataset), exist_ok=True)
 
     def test(self):
@@ -63,7 +63,7 @@ class Inference():
                         output = (output.squeeze().detach().cpu().numpy() * 255.0).astype(np.uint8)
 
                         salient_object = self.post_processing(images[i], output, h, w)
-                        cv2.imwrite(os.path.join('data', 'tracer_mask', image_name[i] + '.png'), output)
+                        cv2.imwrite(os.path.join('data', 'tracer_results', image_name[i] + '.png'), output)
                         # cv2.imwrite(os.path.join('data', self.args.dataset, image_name[i] + '.png'), salient_object)
 
         print(f'time: {time.time() - t:.3f}s')
